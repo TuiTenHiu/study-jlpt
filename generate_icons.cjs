56bf40f2c4eac35a0ef19b8256f5354837816db5
trunghieu0608 <104688403+trunghieu0608@users.noreply.github.com>
2026-09-15 23:20:26 +0700

    commit 16/9

---
+++ generate_icons.cjs
@@ -0,0 +1,24 @@
+﻿const sharp = require('sharp');
+const fs = require('fs');
+
+async function generate() {
+    try {
+        const svgBuffer = fs.readFileSync('icon.svg');
+        
+        await sharp(svgBuffer)
+            .resize(192, 192)
+            .png()
+            .toFile('icon-192.png');
+            
+        await sharp(svgBuffer)
+            .resize(512, 512)
+            .png()
+            .toFile('icon-512.png');
+            
+        console.log('Successfully generated icon-192.png and icon-512.png');
+    } catch (err) {
+        console.error('Error:', err);
+    }
+}
+
+generate();
